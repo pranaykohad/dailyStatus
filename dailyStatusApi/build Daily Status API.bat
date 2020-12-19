e7d8aa9d2832252a3567875bec5757ac33f347d2
pranay.kohad <pkohad@xpanxion.com>
2020-12-19 13:29:19 +0530

    ignore bat file

--- dailyStatusApi/build Daily Status API.bat
+++ dailyStatusApi/build Daily Status API.bat
@@ -2,5 +2,5 @@ g:
 cd G:\DailyStatusSystem\DailyStatusApi
 call mvn clean install -DskipTests
 cd target
-call java -jar dailyStatusApi-0.0.1-SNAPSHOT.jar
+call java -jar dailyStatusApi-1.0-SNAPSHOT.jar
 pause
