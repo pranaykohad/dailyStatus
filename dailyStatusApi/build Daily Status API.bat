18ceb0521e3436ad5fb479b9c5d9ddc7d390b31c
pranaykohad <prakohad@gmail.com>
2020-12-19 13:02:30 +0530

    renamed project

--- dailyStatusApi/build Daily Status API.bat
+++ dailyStatusApi/build Daily Status API.bat
@@ -1,5 +1,5 @@
-d:
-cd D:\PersonnalSpringBoot\DailyStatus\dailyStatus\dailyStatusApi
+g:
+cd G:\DailyStatusSystem\DailyStatusApi
 call mvn clean install -DskipTests
 cd target
 call java -jar dailyStatusApi-0.0.1-SNAPSHOT.jar
