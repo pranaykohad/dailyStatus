683a9f5aae0c837a89493c079dfd37714da1b2f4
pranay.kohad <pkohad@xpanxion.com>
2021-04-25 20:32:49 +0530

    #comment formmating

--- StatusHubApi/src/main/java/com/statushub/main/MainComponent.java
+++ StatusHubApi/src/main/java/com/statushub/main/MainComponent.java
@@ -18,14 +18,14 @@ import com.statushub.entity.User;
 import com.statushub.service.SettingService;
 import com.statushub.service.UserService;
 
-@SpringBootApplication(scanBasePackages="com")
+@SpringBootApplication(scanBasePackages = "com")
 @EntityScan("com.statushub.entity")
 @EnableJpaRepositories("com.statushub.repository")
 public class MainComponent {
 
 	@Autowired
 	UserService userService;
-	
+
 	@Autowired
 	SettingService settingService;
 
@@ -37,15 +37,15 @@ public class MainComponent {
 	}
 
 	@PostConstruct
-	public void init(){
-		if(userService.userCount() == 0) {
+	public void init() {
+		if (userService.userCount() == 0) {
 			addDefaultUser();
 			addDefaultSettings();
 		}
 	}
 
 	private void addDefaultUser() {
-		final User adminUser = new User(); 
+		final User adminUser = new User();
 		adminUser.setFirstName("admin");
 		adminUser.setLastName("admin");
 		adminUser.setModuleName("Not Applicable");
@@ -65,51 +65,47 @@ public class MainComponent {
 	private void addDefaultSettings() {
 		final List<Setting> settingList = new ArrayList<>();
 		Setting setting = new Setting();
-		
+
 		setting.setKeyName("PLANNED_LEAVE_DIFFRENCE");
-		setting.setValue("1");
+		setting.setValue("7");
 		settingList.add(setting);
-		
+
 		setting = new Setting();
 		setting.setKeyName("START_HOUR");
 		setting.setValue("8");
 		settingList.add(setting);
-		
+
 		setting = new Setting();
 		setting.setKeyName("END_HOUR");
 		setting.setValue("17");
 		settingList.add(setting);
-		
+
 		setting = new Setting();
 		setting.setKeyName("MODULE_LIST");
 		setting.setValue("OCR,Connector,Workbench 9.2,Portal,Automation");
 		settingList.add(setting);
-		
+
 		setting = new Setting();
 		setting.setKeyName("USER_TYPE_LIST");
 		setting.setValue("DEV,QA,AQA,PQA");
 		settingList.add(setting);
-		
+
 		setting = new Setting();
 		setting.setKeyName("POSITION_LIST");
-		setting.setValue("PgM,Project Lead,Sr. Architech,Sr. Tech Lead,Lead Developer,Developer,Sr. Developer,Perf. Eng.,Lead QA,QA,Sr. AQA,AQA");
+		setting.setValue(
+				"PgM,Project Lead,Sr. Architech,Sr. Tech Lead,Lead Developer,Developer,Sr. Developer,Perf. Eng.,Lead QA,QA,Sr. AQA,AQA");
 		settingList.add(setting);
-		
+
 		setting = new Setting();
 		setting.setKeyName("FULL_DAY_COLOR");
 		setting.setValue("#28a745");
 		settingList.add(setting);
-		
-		setting = new Setting();
-		setting.setKeyName("FULL_DAY_COLOR");
-		setting.setValue("#28a745");
-		settingList.add(setting);
-		
+
 		setting = new Setting();
 		setting.setKeyName("HALF_DAY_COLOR");
 		setting.setValue("#abc42d");
 		settingList.add(setting);
-		
+
 		settingService.saveOrUpdateSetting(settingList);
 	}
 
